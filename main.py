1d7cbce2d94554f221c3cb2f779ee2d307d49330
Anton K <fergot.nn@gmail.com>
2023-04-29 22:24:48 +0300

    fix conflict

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from PIL import Image
 import argparse
 
 parser = argparse.ArgumentParser(description='PNG to JPG console converter')
-parser.add_argument("--input", type=str, help='File for convert')
-parser.add_argument("--output", type=str, help='Output file')
+parser.add_argument('-i', '--input', type=str, required=True, help='File for convert')
+parser.add_argument('-o', '--output', type=str, required=True, help='Output file')
 args = parser.parse_args()
 _in = args.input
 _out = args.output
 
-im = Image.open(_in)
-rgb_im = im.convert('RGB')
-rgb_im.save(_out)
+try:
+    im = Image.open(_in)
+    rgb_im = im.convert('RGB')
+    rgb_im.save(_out)
+    im.close()
+except Exception as error:
+    print(error)
